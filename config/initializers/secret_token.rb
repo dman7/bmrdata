0549cf3f31fbf6edf9f34a3cb480c1434add93d4
Dmitri Skjorshammer <dmitriskj@gmail.com>
2013-09-11 12:46:41 -0700

    Initial commit; new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmrdata::Application.config.secret_token = 'ecaae77722ddb406e14d8e9cacf0408c71fadff8eadf476a00dd9b85f97c4220cc9a77365d0a9d6ce93f165c64f8b73847cd4700a144d57e267d2e56c4c13628'
